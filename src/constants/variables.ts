331194d111c1d69fffe496a60b1e19f3ab191a2b
pewww <jung22337@naver.com>
2021-02-17 19:26:54 +0900

    :sparkles: DiffStat 관련 색상 상수 및 enum 추가

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -5,3 +5,6 @@ export const IMAGE_SIZE = 14;
 export const $BORDER = '#e1e4e8';
 export const $SKY = '#0c79f8';
 export const $WHITE = '#fff';
+export const $DIFFSTAT_ADDED = '#28a745';
+export const $DIFFSTAT_DELETED = '#d73a49';
+export const $DIFFSTAT_NEUTRAL = '#d1d5da';
---
+++ src/enums/DiffStat.ts
@@ -0,0 +1,5 @@
+export enum CustomDiffStatType {
+  'diffstat-block-added' = 'added',
+  'diffstat-block-deleted' = 'deleted',
+  'diffstat-block-neutral' = 'neutral'
+}
