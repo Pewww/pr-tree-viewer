b51e653de24ea313d70c59fa2c3126ab2723f1b8
Pewww <jung22337@naver.com>
2021-02-27 20:18:25 +0900

    :sparkles: 익스텐션이 스크롤 범위를 벗어났을 때 위치가 고정되도록 수정

--- src/models/PRTreeViewer.ts
+++ src/models/PRTreeViewer.ts
@@ -21,6 +21,10 @@ export default class PrTreeViewer {
     this.setResizeObserver();
   }
 
+  private get rootElement() {
+    return document.getElementById('pr-tree-viewer-root');
+  }
+
   private get renderedResult() {
     return [
       this.viewed.render(),
@@ -33,15 +37,35 @@ export default class PrTreeViewer {
     return target.classList.contains('js-diff-progressive-container');
   }
 
+  private checkIsToolBarClassChanged(mutation: MutationRecord) {
+    return mutation.type === 'attributes'
+      && mutation.oldValue !== (mutation.target as HTMLElement).className;
+  }
+
+  private changeRootElementPosition(target: HTMLElement) {
+    const rootElement = this.rootElement;
+
+    if (target.classList.contains('is-stuck')) {
+      rootElement.classList.add('is-pr-tree-viewer-stuck');
+    } else {
+      rootElement.classList.remove('is-pr-tree-viewer-stuck');
+    }
+  }
+
   private setMutationObserver() {
     const filesBucketElement = document.getElementById('files_bucket');
+    const prToolBar = document.getElementsByClassName('pr-toolbar')[0];
 
-    if (!filesBucketElement) {
+    if (!filesBucketElement || !prToolBar) {
       return;
     }
 
     this.mutationObserver = new MutationObserver(mutations => {
       mutations.forEach(mutation => {
+        if (this.checkIsToolBarClassChanged(mutation)) {
+          this.changeRootElementPosition(mutation.target as HTMLElement);
+        }
+
         if (this.checkIsDiffContainer(mutation.target as HTMLElement)) {
           this.render();
         }
@@ -52,29 +76,39 @@ export default class PrTreeViewer {
       childList: true,
       subtree: true
     });
+
+    this.mutationObserver.observe(prToolBar, {
+      attributes: true,
+      attributeFilter: ['class'],
+      attributeOldValue: true
+    });
   }
 
   private checkIsPrTreeViewerRoot(target: HTMLElement) {
     return target.getAttribute('id') === 'pr-tree-viewer-root';
   }
 
-  private setDiffContainerWidthAgain(size: readonly ResizeObserverSize[]) {
-    const diffContainer = document.querySelector('#pr-tree-viewer-root + .js-diff-progressive-container') as HTMLElement;
+  private setDiffContainersWidthAgain(size: readonly ResizeObserverSize[]) {
+    const diffContainers = document.querySelectorAll(
+      '#pr-tree-viewer-root ~ .js-diff-progressive-container'
+    ) as NodeListOf<HTMLElement>;
     const [{ inlineSize }] = size;
     const sizeBuffer = 10;
 
-    if (!diffContainer) {
+    if (isEmpty(diffContainers)) {
       return;
     }
 
-    diffContainer.style.width = `calc(100% - ${inlineSize + sizeBuffer}px)`;
+    diffContainers.forEach(diffContainer => {
+      diffContainer.style.width = `calc(100% - ${inlineSize + sizeBuffer}px)`; 
+    });
   }
 
   private setResizeObserver() {
     this.resizeObserver = new ResizeObserver(mutations => {
       mutations.forEach(mutation => {
         if (this.checkIsPrTreeViewerRoot(mutation.target as HTMLElement)) {
-          this.setDiffContainerWidthAgain(mutation.borderBoxSize);
+          this.setDiffContainersWidthAgain(mutation.borderBoxSize);
         }
       });
     });
@@ -87,8 +121,8 @@ export default class PrTreeViewer {
       return;
     }
 
-    if (document.getElementById('pr-tree-viewer-root')) {
-      const rootElement = document.getElementById('pr-tree-viewer-root');
+    if (this.rootElement) {
+      const rootElement = this.rootElement;
 
       this.resizeObserver.observe(rootElement);
 
