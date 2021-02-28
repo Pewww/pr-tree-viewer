eed27587b392ad36fc7b04e576c2486b459c9a1c
Pewww <jung22337@naver.com>
2021-02-28 16:19:59 +0900

    :sparkles: Model 간의 통신을 위한 EventBus 추가

---
+++ src/models/EventBus.ts
@@ -0,0 +1,52 @@
+import isEmpty from 'lodash.isempty';
+
+import { getRandomId } from '../lib/random';
+
+interface ISubscriptions {
+  [type: string]: {
+    [id: string]: (arg: unknown) => void;
+  };
+}
+
+class EventBus {
+  private subscriptions: ISubscriptions;
+
+  constructor() {
+    this.subscriptions = {};
+  }
+
+  public on<ArgType>(type: string, callback: (arg: ArgType) => void) {
+    const id = getRandomId();
+
+    if (!this.subscriptions[type]) {
+      this.subscriptions[type] = {};
+    }
+
+    this.subscriptions[type][id] = callback;
+
+    return {
+      unsubscribe: () => {
+        delete this.subscriptions[type][id];
+
+        if (isEmpty(this.subscriptions[type])) {
+          delete this.subscriptions[type];
+        }
+      }
+    };
+  }
+
+  public emit<ArgType>(type: string, arg: ArgType) {
+    if (!this.subscriptions[type]) {
+      return;
+    }
+
+    Object.keys(this.subscriptions[type])
+      .forEach(id => {
+        this.subscriptions[type][id](arg);
+      });
+  }
+}
+
+const $eventBus = new EventBus();
+
+export default $eventBus;
