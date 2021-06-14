75ab6017b2d58a565310222bcd44b730869e8662
hyperflow <hyperflow@kakao.com>
2021-06-15 00:28:43 +0900

    Fix issue about github beta feature

--- src/models/ChangedFiles.ts
+++ src/models/ChangedFiles.ts
@@ -41,7 +41,7 @@ export default class ChangedFiles {
 
   private getFileSrcs() {
     const fileSrcTags = Array.from(
-      document.querySelectorAll(`.${this.rootClassName} > a`)
+      document.querySelectorAll(`.${this.rootClassName} > .diffstat + a`)
     );
 
     return fileSrcTags.map(({ title }: HTMLElement) =>
