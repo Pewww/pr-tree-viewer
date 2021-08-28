110049df2ed8223f21f9ecaeb4316bf257fd08aa
pewww <jung22337@naver.com>
2021-08-28 16:54:31 +0900

    :construction: 작업 내용 임시 저장

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import $eventBus from './models/EventBus';
 import { loadFonts } from './lib/fonts';
 import './index.css';
 
+// https://github.com/berzniz/github_pr_tree/blob/master/src/js/index.jsx#L13
 const start = () => {
   // ID가 계속 쌓이는것을 방지하기 위해 인스턴스 생성 전 초기화
   $eventBus.off('remove');
@@ -11,7 +12,27 @@ const start = () => {
   prTreeViewer.render();
 };
 
-chrome.runtime.onMessage.addListener(() => {
-  start();
-  loadFonts();
-});
+const pjaxContainer = document.querySelector('[data-pjax-container]');
+const repoContentPjaxContainer = document.getElementById('repo-content-pjax-container');
+
+const rootMutationObserver = new MutationObserver(start);
+
+if (pjaxContainer) {
+  rootMutationObserver.observe(pjaxContainer, {
+    childList: true
+  });
+}
+
+if (repoContentPjaxContainer) {
+  rootMutationObserver.observe(repoContentPjaxContainer, {
+    childList: true
+  });
+}
+
+start();
+loadFonts();
+
+// chrome.runtime.onMessage.addListener(() => {
+//   start();
+//   loadFonts();
+// });
